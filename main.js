d6b83121a0358ca65fa7b1eee632443a4da510f9
Dakota Larson <dakotalarson1996@gmail.com>
2018-04-27 12:57:28 -0500

    Small Update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const three = require('three');
 let scene = new three.Scene();
 let camera = new three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+scene.background = new three.Color(0x87CEEB);
 let renderer = new three.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.domElement.id = 'game-canvas';
@@ -13,19 +13,15 @@ window.addEventListener('resize', function(){
     renderer.setSize( window.innerWidth, window.innerHeight );
 });
 
-let geometry = new three.BoxGeometry( 1, 1, 1 );
-let material = new three.MeshBasicMaterial( { color: 0x00ff00 } );
-let cube = new three.Mesh( geometry, material );
-scene.add( cube );
+let geometry = new three.PlaneGeometry(25, 25);
+let material = new three.MeshBasicMaterial({color:0x99be90});
+let mesh = new three.Mesh(geometry, material);
+mesh.rotateX(-Math.PI / 2);
+scene.add(mesh);
 
-camera.position.z = 5;
-
+camera.position.y = 5;
 let animate = function () {
     requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
     renderer.render(scene, camera);
 };
 
